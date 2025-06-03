592c5a5c515009bb8b7733197447b69ed1452b35
lwam <lwambsrat@gmail.com>
2025-06-03 15:25:43 +0300

    doc: Added javascript dom code align with html,and css

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 document.body.style.backgroundColor = "silver";
 const title=document.getElementById("title");
-const vegetable=document.getElementById("vegetable");
+const vegetable=document.getElementById("spinach");
+const apple=document.getElementById("apple");
 const paragraph=document.getElementById("paragraph");
+const fruit=document.getElementById("banana");
+const tomato=document.getElementById("tomato");
+tomato.style.width="15vw";
+tomato.style.height="33vh";
+tomato.style.marginLeft="55%";
+tomato.style.borderRadius="9cqb";
 paragraph.style.marginTop="7%";
 paragraph.style.marginLeft="35%";
 paragraph.style.fontSize="40px";
-const fruit=document.getElementById("banana");
+apple.style.width="80%";
+apple.style.marginLeft="99%";
+apple.style.marginTop="-25%";
 fruit.style.height="50%";
 fruit.style.width="60%";
 fruit.style.marginLeft="40%";
@@ -13,7 +22,6 @@ fruit.style.marginTop="-10%";
 vegetable.style.width="90%";
 vegetable.style.marginLeft="89%";
 title.style.color="green";
-
 title.style.marginLeft="35%";
 title.style.fontSize="70px";
 
@@ -40,4 +48,4 @@ var newVeg = document.createElement("li");
 newVeg.textContent = "Spinach";
 vegList.appendChild(newVeg);
 vegList.style.marginLeft="69%";
-vegList.style.fontSize="30px"
+vegList.style.fontSize="30px";
